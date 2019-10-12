2c18e8804041eab0989afd3e5d28159bce5838a3
Sean Robertson <seanrobertson@improbable.io>
2019-10-12 16:15:22 +0100

    Allow extra properties in pipelines, with a test

--- test/schema.test.js
+++ test/schema.test.js
@@ -19,11 +19,20 @@ const validate = (name) => {
 }
 
 describe('schema.json', function() {
+  it('should validate block steps', function () {
+    validate('block.yml')
+  })
   it('should validate command steps', function() {
     validate('command.yml')
   })
-  it('should validate block steps', function() {
-    validate('block.yml')
+  it('should validate env blocks', function () {
+    validate('env.yml')
+  })
+  it('should validate blocks with extra properties', function () {
+    validate('extra-properties.yml')
+  })
+  it('should validate step groups', function () {
+    validate('group.yml')
   })
   it('should validate trigger steps', function() {
     validate('trigger.yml')
