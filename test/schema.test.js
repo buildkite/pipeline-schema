60c64a7f38131527ce6654e10354693392286b10
Zack Chase <zachary@buildkite.com>
2025-09-03 10:25:18 -0700

    Fix quotes

--- test/schema.test.js
+++ test/schema.test.js
@@ -19,42 +19,42 @@ const validate = (name) => {
   }
 };
 
-describe('schema.json', function() {
-  it('should validate block steps', function () {
-    validate('block.yml')
+describe("schema.json", function() {
+  it("should validate block steps", function () {
+    validate("block.yml")
   })
-  it('should validate input steps', function () {
-    validate('input.yml')
+  it("should validate input steps", function () {
+    validate("input.yml")
   })
-  it('should validate command steps', function() {
-    validate('command.yml')
+  it("should validate command steps", function() {
+    validate("command.yml")
   })
-  it('should validate env blocks', function () {
-    validate('env.yml')
+  it("should validate env blocks", function () {
+    validate("env.yml")
   })
-  it('should validate blocks with extra properties', function () {
-    validate('extra-properties.yml')
+  it("should validate blocks with extra properties", function () {
+    validate("extra-properties.yml")
   })
-  it('should validate step groups', function () {
-    validate('group.yml')
+  it("should validate step groups", function () {
+    validate("group.yml")
   })
-  it('should validate trigger steps', function() {
-    validate('trigger.yml')
+  it("should validate trigger steps", function() {
+    validate("trigger.yml")
   })
-  it('should validate wait steps', function() {
-    validate('wait.yml')
+  it("should validate wait steps", function() {
+    validate("wait.yml")
   })
-  it('should validate notify', function() {
-    validate('notify.yml')
+  it("should validate notify", function() {
+    validate("notify.yml")
   })
-  it('should validate matrix', function() {
-    validate('matrix.yml')
+  it("should validate matrix", function() {
+    validate("matrix.yml")
   })
 
-  it('should verify groupStep.steps uses the same-ish items as root steps', function () {
+  it("should verify groupStep.steps uses the same-ish items as root steps", function () {
     const mainList = schema.definitions.pipelineSteps.items.anyOf
     const groupList = schema.definitions.groupSteps.items.anyOf
     expect(mainList.slice(0, -1)).to.eql(groupList)
-    expect(mainList[mainList.length - 1].$ref).to.eql('#/definitions/groupStep')
+    expect(mainList[mainList.length - 1].$ref).to.eql("#/definitions/groupStep")
   })
 })
